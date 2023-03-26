0e90b9cc323b5fab3bfd86c0a0f7a1f318ac30e7
gabrielmiki <96574190+gabrielmiki@users.noreply.github.com>
2023-03-26 06:54:53 -0300

    Added First Angular Blog Project

---
+++ Projects/first-angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    'id':'0',
+    'photo':'https://s2.glbimg.com/n3hGOvRC64SO_OG4Jh516HDm2m8=/e.glbimg.com/og/ed/f/original/2015/10/26/criatividade-lampada-ideias.jpg',
+    'title':'Ideias Contagiosas!',
+    'description':'Ideias que mudaram o mundo e a forma de interação entre pessoas.'
+  },
+  {
+    'id':'1',
+    'photo':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnZq4iVoUab59g9uW0gUT0aYF7fOeh2xEYkQ&usqp=CAU',
+    'title':'Sem medo do fracasso! Histórias para se motivar.',
+    'description':'asaoisjodicjaodij'
+  }
+];
